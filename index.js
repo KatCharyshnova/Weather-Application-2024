fbd11047d9fb9de7afa12d532867a3bafe4a4719
KatCharyshnova <ch.k.charyshnova@gmail.com>
2024-02-19 12:48:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
-function showTemperature(response) {
-  let cityElement = document.querySelector("#current-city");
-  let cityEntered = response.data.city;
-  let tempElement = document.querySelector(".current-temperature-value");
+function displayTemp(response) {
+  let temperatureElement = document.querySelector("#tempReading");
   let temperature = Math.round(response.data.temperature.current);
-  tempElement.innerHTML = temperature;
-  cityElement.innerHTML = cityEntered;
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
 }
 
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-  let apiKey = `1a747f2d7ac32a100bt13fab8776o6ca`;
+  let searchInput = document.querySelector("#search-text-input");
+  let city = searchInput.value;
+  let apiKey = "dd9ad0073b1c71eoafbt4187fa1bef0d";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(displayTemp);
 }
 
+let now = new Date();
+
 function formatDate(date) {
-  let minutes = date.getMinutes();
-  let hours = date.getHours();
-  let day = date.getDay();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
   let days = [
     "Sunday",
     "Monday",
@@ -37,15 +26,28 @@ function formatDate(date) {
     "Thursday",
     "Friday",
     "Saturday",
+    "Sunday",
   ];
 
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
-}
+  let currentDay = days[now.getDay()];
+  let currentHour = now.getHours();
+  let currentMinutes = now.getMinutes();
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+
+  if (currentHour < 10) {
+    currentHour = `0${currentHours}`;
+  }
+
+  return `${currentDay} ${currentHour}:${currentMinutes}`;
+}
 
 let currentDateElement = document.querySelector("#current-date");
 let currentDate = new Date();
+
 currentDateElement.innerHTML = formatDate(currentDate);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
